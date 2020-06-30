35b450facbc957a9e276ef5c00575ad6224147e8
Federico Vincenzo Mastellone <mastellonefederico@gmail.com>
2020-06-30 22:29:11 +0200

    Modifiche

--- neurone.py
+++ neurone.py
@@ -13,6 +13,7 @@ tau = 144*ms
 a = 4*nsiemens
 b = 0.0805*namp
 sigma = 0.6*namp*(ms**0.5)
+duration = 500*ms
 
 # I corrente esterna
 eqs = '''
@@ -27,6 +28,9 @@ neuroneexcit = NeuronGroup(1, eqs, threshold='v>20*mV', reset='v=El;w=w+b', meth
 monitorinhib = StateMonitor(neuroneinhib, 'v', record=True)
 monitorexcit = StateMonitor(neuroneexcit, 'v', record=True)
 
+spikesinhib = SpikeMonitor(neuroneinhib)
+spikesexcit = SpikeMonitor(neuroneexcit)
+
 
 neuroneinhib.v = El
 neuroneexcit.v = El
@@ -39,15 +43,27 @@ S2 = Synapses(neuroneinhib, neuroneexcit, 'z : 1', on_pre='v_post += -20*mV')
 S2.connect(i=0,j=0)
 S2.delay = '2*ms'
 
-neuroneexcit.run_regularly('I = rand()*3*nA', dt=10*ms)
-run(130*ms)
+arrayI = []
+outputrates = []
 
-plt.figure("Inhib and excit")
-plt.subplot(211)
+for l in range(10):
+    neuroneexcit.I = rand()*3*nA
+    arrayI.append(neuroneexcit.I)
+    outputrates.append(spikesexcit.num_spikes/second)
+    run(50*ms)
+
+plt.figure("Wow")
+plt.subplot(221)
+for l in range(10):
+    axvline(l*50, ls='--', c='b')
 plt.plot(monitorinhib.t/ms, monitorinhib.v.T/mV,'b')
 ylabel("V (mV) Inhibitory")
-plt.subplot(212)
+plt.subplot(223)
 plt.plot(monitorexcit.t/ms, monitorexcit.v.T/mV,'g')
+for l in range(10):
+    axvline(l*50, ls='--', c='g')
 xlabel("Tempo in ms")
 ylabel("V (mV) Excitatory")
+plt.subplot(224)
+plt.plot(arrayI/nA, outputrates, 'og')
 plt.show()
