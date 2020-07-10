150816973a1166b0557ba4b3e8cb11834c03c1ea
Federico Vincenzo Mastellone <federico@neurofedora.station>
2020-07-10 16:21:57 +0200

    primo esercizio

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+import brian2 as b2
+import matplotlib.pyplot as plt
+import numpy as np
+from neurodynex.leaky_integrate_and_fire import LIF
+from neurodynex.tools import input_factory, plot_tools
+
+
+LIF.getting_started()
+LIF.print_default_parameters()
