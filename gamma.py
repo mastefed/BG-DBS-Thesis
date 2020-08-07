ac9a10e8b668db224f04cbabb3ff44cc9b8d8ed1
Federico Vincenzo Mastellone <mastellonefederico@gmail.com>
2020-08-07 13:22:25 +0200

    Update

--- gamma.py
+++ gamma.py
@@ -6,8 +6,9 @@ N_inhi = 1000
 N_exci = 4000
 duration = 10*ms
 
+dim_corr = 1*mV/(volt*second)
+
 p = 0.2 # connectivity probability
-w = 1
 
 v_rest = 0*mV # resting potential
 v_thre = 18*mV # threshold potential
@@ -29,58 +30,68 @@ tl = 1*ms # latency of post-synaptic current
 tdg = 5*ms # decay time of gaba current
 trg = 0.25*ms # rise time of gaba current
 
-# Synaptic efficacies int/pyr/ext to GABA
-j_int_inhi = 2.7*mV
-j_pyr_inhi = 0.7*mV
+# Synaptic efficacies inhi/exci/ext to inhi
+j_inhi_inhi = 2.7*mV
+j_exci_inhi = 0.7*mV
 j_ext_inhi = 0.95*mV
 
-# Synaptic efficacies int/pyr/ext to AMPA
-j_int_exci = 1.7*mV
-j_pyr_exci = 0.42*mV
+# Synaptic efficacies inhi/exci/ext to exci
+j_inhi_exci = 1.7*mV
+j_exci_exci = 0.42*mV
 j_ext_exci = 0.55*mV
 
 # Modello le equazioni per le reti di neuroni inibitori ed eccitatori
 eqs_inhi = '''
 dv/dt = (-v + I_a - I_g)/tm_inhi : volt (unless refractory)
-dI_a/dt = (-I_a + X_a)/tda_inhi : volt
-dI_g/dt = (-I_g + X_g)/tdg : volt
+dI_a/dt = (-I_a + X_a_tot + X_ext_tot)/tda_inhi : volt
+dI_g/dt = (-I_g + X_g_tot)/tdg : volt
 X_a_tot : volt
 X_g_tot : volt
+X_ext_tot : volt
 '''
 
 # La differenza me la ritrovo nelle costanti temporali
 eqs_exci = '''
 dv/dt = (-v + I_a - I_g)/tm_exci : volt (unless refractory)
-dI_a/dt = (-I_a + X_a)/tda_exci : volt
-dI_g/dt = (-I_g + X_g)/tdg : volt
+dI_a/dt = (-I_a + X_a_tot + X_ext_tot)/tda_exci : volt
+dI_g/dt = (-I_g + X_g_tot)/tdg : volt
 X_a_tot : volt
 X_g_tot : volt
+X_ext_tot : volt
 '''
 
 # Modello le equazioni per le correnti sinaptiche
-eqs_pre_inhi = '''
-dX_a/dt = (-X_a + tm_inhi*(j_pyr_inhi*x1 + j_ext_inhi*x2))/tra_inhi : weber (event-driven)
-dX_g/dt = (-X_g + tm_inhi*j_int_inhi*x3)/trg : weber (event-driven)
-X_a_tot_post = X_a : 1 (summed)
-X_g_tot_post = X_g : 1 (summed)
-x1 : 1
-x2 : 1
-x3 : 1
-w : 1
+eqs_exci_to_inhi = '''
+dX_a/dt = -X_a/tra_inhi : volt (event-driven)
+X_a_tot_post = X_a : volt (summed)
 '''
 
-eqs_pre_exci = '''
-dX_a/dt = (-X_a + tm_exci*(j_pyr_exci*x1 + j_ext_exci*x2))/tra_exci : weber (event-driven)
-dX_g/dt = (-X_g + tm_exci*j_int_exci*x3)/trg : weber (event-driven)
-X_a_tot_post = X_a : 1 (summed)
-X_g_tot_post = X_g : 1 (summed)
-x1 : 1
-x2 : 1
-x3 : 1
-w : 1
+eqs_inhi_to_exci = '''
+dX_g/dt = -X_g/trg : volt (event-driven)
+X_g_tot_post = X_g : volt (summed)
 '''
 
+eqs_exci_to_exci = '''
+dX_a/dt = -X_a/tra_exci : volt (event-driven)
+X_a_tot_post = X_a : volt (summed)
+'''
 
+eqs_inhi_to_inhi = '''
+dX_g/dt = -X_g/trg : volt (event-driven)
+X_g_tot_post = X_g : volt (summed)
+'''
+
+eqs_ext_to_inhi = '''
+dX_ext/dt = -X_ext/tra_inhi : volt (event-driven)
+X_ext_tot_post = X_ext : volt (summed)
+'''
+
+eqs_ext_to_exci = '''
+dX_ext/dt = -X_ext/tra_exci : volt (event-driven)
+X_ext_tot_post = X_ext : volt (summed)
+'''
+
+# Modello le reti di neuroni che mi servono
 I = NeuronGroup(N_inhi, eqs_inhi, threshold='v>v_thre', reset='v=v_rese',
                     refractory=tr_inhi, method='exact')
 I.v = v_rest
@@ -89,8 +100,32 @@ E = NeuronGroup(N_exci, eqs_exci, threshold='v>v_thre', reset='v=v_rese',
                     refractory=tr_exci, method='exact')
 E.v = v_rest
 
-EE = Synapses(E, E, model = eqs_pre_exci, delay=tl, on_pre='x1 += w')
+EE = Synapses(E, E, model=eqs_exci_to_exci, delay=tl, on_pre='X_a += tm_exci*j_exci_exci*dim_corr')
+II = Synapses(I, I, model=eqs_inhi_to_inhi, delay=tl, on_pre='X_g += tm_inhi*j_inhi_inhi*dim_corr')
+EI = Synapses(E, I, model=eqs_exci_to_inhi, delay=tl, on_pre='X_a += tm_exci*j_exci_inhi*dim_corr')
+IE = Synapses(I, E, model=eqs_inhi_to_exci, delay=tl, on_pre='X_g += tm_inhi*j_inhi_inhi*dim_corr')
 
+EE.connect(p=p)
+EI.connect(p=p)
+IE.connect(p=p)
+II.connect(p=p)
+
+# Provo a modellare il noise seguendo la guida di Brian2 sui PoissonGroup
+
+sigma_n = 0.4*Hz
+tau_n = 16*ms
+ni_0 = 2*Hz
+
+rates = '''
+ni = ni_0 + n : Hz
+dn/dt = (-n + sigma_n*xi*sqrt(2/tau_n))/tau_n : Hz
+'''
+
+Ext = NeuronGroup(N_exci, rates, threshold='rand()<ni*dt')
+ExtE = Synapses(Ext, E, model=eqs_ext_to_exci, delay=tl, on_pre='X_ext += tm_exci*j_ext_exci*dim_corr')
+ExtI = Synapses(Ext, I, model=eqs_ext_to_inhi, delay=tl, on_pre='X_ext += tm_exci*j_ext_inhi*dim_corr')
+ExtE.connect()
+ExtI.connect()
 
 M_inhi = SpikeMonitor(I)
 M_exci = SpikeMonitor(E)
