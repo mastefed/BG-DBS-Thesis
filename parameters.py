cf7a4d29a9e683c5837bf2ef70dcecd48bf6836c
Federico Vincenzo Mastellone <mastellonefederico@gmail.com>
2021-01-11 08:54:38 +0000

    Got correct poisson input parameters

--- parameters.py
+++ parameters.py
@@ -39,13 +39,13 @@ neuronparameters = {
 
 # Poisson noise
 poissoninput = {
-  'FSN' : {'num' : 210, 'rate' : 1000., 'weight' : 1.45, 'delay' : 1.}, # 1000
-  'D1' : {'num' : 5000, 'rate' : 2930., 'weight' : 1.45, 'delay' : 1.}, # 2930 
+  'FSN' : {'num' : 210, 'rate' : 850., 'weight' : 1.45, 'delay' : 1.}, # 1000
+  'D1' : {'num' : 5000, 'rate' : 4000., 'weight' : 1.45, 'delay' : 1.}, # 2930 3480
   'D2' : {'num' : 5000, 'rate' : 2560., 'weight' : 1.45, 'delay' : 1.}, # 2560
-  'STN' : {'num' : 776, 'rate' : 1690., 'weight' : 1.15, 'delay' : 1.}, # 1690
-  'GPTI' : {'num' : 1976, 'rate' : 1650., 'weight' : 1.25, 'delay' : 1.}, # 1650
-  'GPTA' : {'num' : 658, 'rate' : 330., 'weight' : 0.6, 'delay' : 1.}, # 330
-  'GPI' : {'num' : 1508, 'rate' : 1250., 'weight' : 3.45, 'delay' : 1.} # 1250
+  'STN' : {'num' : 776, 'rate' : 1730., 'weight' : 1.15, 'delay' : 1.}, # 1690
+  'GPTI' : {'num' : 1976, 'rate' : 1800., 'weight' : 1.25, 'delay' : 1.}, # 1650
+  'GPTA' : {'num' : 658, 'rate' : 900., 'weight' : 0.6, 'delay' : 1.}, # 330
+  'GPI' : {'num' : 1508, 'rate' : 1440., 'weight' : 3.45, 'delay' : 1.} # 1250
 }
 
 # Static syn parameters TARGET-SOURCES
